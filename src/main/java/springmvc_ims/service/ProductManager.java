7ccd933c5533fa0d6c4118aca824532a973b4876
Attila Gavallér <attila.gavaller@gmail.com>
2016-12-28 11:44:30 +0100

    chapter 3 done

---
+++ src/main/java/springmvc_ims/service/ProductManager.java
@@ -0,0 +1,13 @@
+package springmvc_ims.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+import springmvc_ims.model.Product;
+
+public interface ProductManager extends Serializable {
+
+    public void increasePrice(int percentage);
+    
+    public List<Product> getProducts();
+}
---
+++ src/test/springmvc_ims/web/HelloControllerTest.java
@@ -0,0 +1,18 @@
+package springmvc_ims.web;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import junit.framework.TestCase;
+
+public class HelloControllerTest extends TestCase {
+
+    public void testHandleRequestView() throws Exception{
+        HelloController controller = new HelloController();
+        ModelAndView modelAndView = controller.handleRequest(null, null);
+        assertEquals("hello", modelAndView.getViewName());
+        assertNotNull(modelAndView.getModel());
+        String nowValue = (String) modelAndView.getModel().get("now");
+        assertNotNull(nowValue);
+    }
+
+}
