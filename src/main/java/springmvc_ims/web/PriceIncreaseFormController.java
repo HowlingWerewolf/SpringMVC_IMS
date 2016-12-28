cae75939ac7f09b8648e251da0bfc7e99d96ebb2
Attila Gavallér <attila.gavaller@gmail.com>
2016-12-28 14:55:04 +0100

    form rendering issue resolved

--- src/main/java/springmvc_ims/web/PriceIncreaseFormController.java
+++ src/main/java/springmvc_ims/web/PriceIncreaseFormController.java
@@ -1,16 +1,13 @@
 package springmvc_ims.web;
 
-import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindException;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,16 +25,9 @@ public class PriceIncreaseFormController {
 
     @Autowired
     private ProductManager productManager;
-
-    @RequestMapping(value = "/priceincrease", method = RequestMethod.GET)    
-//    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//
-//        logger.info("returning priceincrease view");
-//        return new ModelAndView("priceincrease");
-//    }
     
-    public ModelAndView onSubmit(@ModelAttribute("priceIncrease") PriceIncrease command)
+    @RequestMapping(method = RequestMethod.POST) 
+    public ModelAndView onSubmit(@ModelAttribute("priceincrease") PriceIncrease command)
             throws ServletException {
 
         logger.info("Command received: " + command);
@@ -55,6 +45,14 @@ public class PriceIncreaseFormController {
         priceIncrease.setPercentage(20);
         logger.info("priceincrease object set with " + priceIncrease.getPercentage() + "%");
         return priceIncrease;
+    }
+    
+    @RequestMapping(value = "/priceincrease", method = RequestMethod.GET) 
+    public String displayLogin(Model model) { 
+	   
+	   	// very special thanks to this solution!!!  http://stackoverflow.com/questions/8781558/neither-bindingresult-nor-plain-target-object-for-bean-name-available-as-request
+        model.addAttribute("priceincrease", new PriceIncrease()); 
+        return "priceincrease"; 
     }
 
     public void setProductManager(ProductManager productManager) {
