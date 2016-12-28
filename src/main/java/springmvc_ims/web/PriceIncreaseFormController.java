f9dd5de471016904fd2bfbd1086ad4c944c5e990
Attila Gavallér <attila.gavaller@gmail.com>
2016-12-28 14:22:08 +0100

    priceincrease command can be reveived by onsubmit

--- src/main/java/springmvc_ims/web/PriceIncreaseFormController.java
+++ src/main/java/springmvc_ims/web/PriceIncreaseFormController.java
@@ -1,12 +1,17 @@
 package springmvc_ims.web;
 
+import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,14 +29,19 @@ public class PriceIncreaseFormController {
     @Autowired
     private ProductManager productManager;
 
-    @RequestMapping(value = "/priceincrease", method = RequestMethod.GET)
-    public ModelAndView onSubmit(Object command)
+    @RequestMapping(value = "/priceincrease", method = RequestMethod.GET)    
+//    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//
+//        logger.info("returning priceincrease view");
+//        return new ModelAndView("priceincrease");
+//    }
+    
+    public ModelAndView onSubmit(@ModelAttribute("priceIncrease") PriceIncrease command)
             throws ServletException {
 
-//    	TODO: fix command conversion problem
-//        logger.info("Command received: " + command);
-//        int increase = ((PriceIncrease) command).getPercentage();
-    	int increase = 10;
+        logger.info("Command received: " + command);
+        int increase = ((PriceIncrease) command).getPercentage();
         logger.info("Increasing prices by " + increase + "%.");
         productManager.increasePrice(increase);
 
@@ -43,6 +53,7 @@ public class PriceIncreaseFormController {
     protected Object formBackingObject(HttpServletRequest request) throws ServletException {
         PriceIncrease priceIncrease = new PriceIncrease();
         priceIncrease.setPercentage(20);
+        logger.info("priceincrease object set with " + priceIncrease.getPercentage() + "%");
         return priceIncrease;
     }
 
