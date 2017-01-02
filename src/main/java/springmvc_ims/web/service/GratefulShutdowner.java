e3bfc4c884ef8e7b5abe86f12e1f75f2a108351b
Attila Gavallér <attila.gavaller@gmail.com>
2017-01-02 09:56:28 +0100

    deregistering JDBC Drivers on shutdown

--- src/main/java/springmvc_ims/web/service/GratefulShutdowner.java
+++ src/main/java/springmvc_ims/web/service/GratefulShutdowner.java
@@ -1,5 +1,12 @@
 package springmvc_ims.web.service;
 
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.stereotype.Component;
@@ -9,9 +16,32 @@ import springmvc_ims.db.HibernateUtil;
 @Component
 public class GratefulShutdowner implements ApplicationListener<ContextClosedEvent> {
 	
+    /** Logger for this class and subclasses */
+    protected final Log logger = LogFactory.getLog(getClass());
+    
 	@Override
 	public void onApplicationEvent(ContextClosedEvent e) {
-		HibernateUtil.shutdown();
+		shutdownHibernate();
+		deregisterJdbcDrivers();
+	}
+	
+	private void shutdownHibernate() {
+		HibernateUtil.shutdown();		
+	}
+	
+	private void deregisterJdbcDrivers() {
+        // This manually deregisters JDBC driver, which prevents Tomcat 7 from complaining about memory leaks wrto this class
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+        while (drivers.hasMoreElements()) {
+            Driver driver = drivers.nextElement();
+            try {
+                DriverManager.deregisterDriver(driver);
+                logger.info(String.format("deregistering jdbc driver: %s", driver));
+            } catch (SQLException ex) {
+                logger.error(String.format("Error deregistering driver %s", driver));
+            }
+
+        }
 	}
 	
 }
