6bd11b1e8832be977e74d04d6399cc88d1ce00cb
Ati <backdoor.werewolf@gmail.com>
2026-03-23 09:55:03 +0100

    wip

--- core/src/main/java/com/ims/AppConfig.java
+++ core/src/main/java/com/ims/AppConfig.java
@@ -1,6 +1,6 @@
 package com.ims;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
