daee3197fb83a2b1146a7fbcc98b01976e6d956a
Ati <backdoor.werewolf@gmail.com>
2026-03-23 06:58:03 +0100

    chore(frontend): upgrade Angular to 18 and apply migrations

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { ProductsComponent } from './products/products.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
-@NgModule({
-  declarations: [AppComponent, HelloComponent, ProductsComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule, RouterModule.forRoot([
-    { path: '', component: ProductsComponent },
-    { path: 'hello', component: HelloComponent }
-  ])],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [AppComponent, HelloComponent, ProductsComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule, FormsModule, RouterModule.forRoot([
+            { path: '', component: ProductsComponent },
+            { path: 'hello', component: HelloComponent }
+        ])], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
 
