90409024f7c78d34b2d60a0fa7e13467b6ac11ae
Ati <backdoor.werewolf@gmail.com>
2026-03-26 08:09:23 +0100

    wip

--- start.bat
+++ start.bat
@@ -3,20 +3,4 @@ call mvn clean package
 REM jump out if case of error
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-@REM cd web
-@REM call npm install
-@REM
-@REM REM jump out if case of error
-@REM if %errorlevel% neq 0 exit /b %errorlevel%
-@REM
-@REM call npm ci
-@REM
-@REM REM jump out if case of error
-@REM if %errorlevel% neq 0 exit /b %errorlevel%
-@REM
-@REM call npm run build
-@REM
-@REM REM jump out if case of error
-@REM if %errorlevel% neq 0 exit /b %errorlevel%
-
-call docker compose up
+call docker compose up --build
