89a89a78a8b732e72590e27f03063efcee265bd3
HowlingWerewolf <backdoor.werewolf@gmail.com>
2026-04-10 10:11:19 +0200

    Docker cloud preparation (#5)

--- start.bat
+++ start.bat
@@ -1,11 +1,21 @@
-call build_scripts/be_build.bat
+cd web
+call npm install
 
 REM jump out if case of error
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-call build_scripts/fe_build.bat
+call npm ci
 
 REM jump out if case of error
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-call docker compose up --build
+call npm run build
+
+cd ..
+
+call mvn clean package
+
+REM jump out if case of error
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+call docker-compose -f docker-compose.local.yml up -d
